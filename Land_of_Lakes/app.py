2df8ff85ba0fcfc8493d715cd0f81b1adf904fcc
Gavin S <gavinslusher@gmail.com>
2020-05-10 12:46:58 -0400

    Updated routes, installed forms

--- Land_of_Lakes/app.py
+++ Land_of_Lakes/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from .routes.main import main
+from .routes.add import add
 
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(main)
+    app.register_blueprint(add)
 
     return app
--- Land_of_Lakes/routes/main.py
+++ Land_of_Lakes/routes/main.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+# from ..forms import (ClientForm, AdvisorForm, AccountForm, UpdateClient,
+                     # DeleteForm, TablesForm)
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+
+@main.route('/search_database', methods=['GET', 'POST'])
+def search_database():
+    return "search database"
+
+@main.route('/view_tables', methods=['GET', 'POST'])
+def view_tables():
+    return "view_tables"
