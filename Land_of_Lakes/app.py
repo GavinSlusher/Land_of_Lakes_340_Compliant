c12f380793dd0e716ecf81afbf390a1712a1d81d
Gavin S <gavinslusher@gmail.com>
2020-05-10 10:56:41 -0400

    Changes to imports to make run.py work

--- Land_of_Lakes/app.py
+++ Land_of_Lakes/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from routes.main import main
+from .routes.main import main
 
 
 def create_app():
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 # This file will be used to run the app persistently on Flip!
 
-from .Land_of_Lakes.app import app
+from Land_of_Lakes.app import create_app
