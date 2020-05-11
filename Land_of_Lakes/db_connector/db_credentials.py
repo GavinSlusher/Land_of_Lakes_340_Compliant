0d77f418400af8f253b2c7a705aef6c2317f0d57
Gavin S <gavinslusher@gmail.com>
2020-05-11 09:03:24 -0400

    Added bsg_table to index page as a db test

---
+++ Land_of_Lakes/db_connector/db_credentials.py
@@ -0,0 +1,7 @@
+# the following will be used by the db_connector.py and
+# in turn by all the Python code in this codebase to interact with the database
+
+host = 'classmysql.engr.oregonstate.edu'  # DON'T CHANGE
+user = 'cs340_slusherg'  # CHANGE ME
+passwd = '9690'  # CHANGE ME
+db = 'cs340_slusherg'  # CHANGE ME
--- Land_of_Lakes/routes/main.py
+++ Land_of_Lakes/routes/main.py
@@ -1,12 +1,25 @@
 from flask import Blueprint, render_template
 from ..forms import TablesForm
+from ..db_connector.db_connector import connect_to_database, execute_query
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+
+    # Testing db_connection
+
+    db_connection = connect_to_database()
+    query = ("SELECT id, fname, lname, homeworld, age FROM bsg_people "
+             "ORDER BY id;")
+
+    print(query)
+
+    rows = execute_query(db_connection, query).fetchall()
+    print(rows)
+
+    return render_template('index.html', rows=rows)
 
 
 @main.route('/search_database', methods=['GET', 'POST'])
