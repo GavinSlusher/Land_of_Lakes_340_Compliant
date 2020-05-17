63133a3a847f267d13cff55ffd7bd29170434c56
Gavin S <gavinslusher@gmail.com>
2020-05-17 14:36:07 -0400

    Adding LoL Definition SQL file

---
+++ DDL_and_DML/LoL_Data_Definition.sql
@@ -0,0 +1,72 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE `clients` (
+  `client_id` int NOT NULL AUTO_INCREMENT,
+  `ssn` int NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address_id` int,
+  `client_account_id` int,
+  `client_advisor_id` int,
+  PRIMARY KEY (`client_id`),
+  FOREIGN KEY (`address_id`) NOT NULL REFERENCES `addresses`(`address_id`)
+  FOREIGN KEY ('client_account_id') NOT NULL REFERENCES 'clients_accounts'('clients_accounts'),
+  FOREIGN KEY ('client_advisor_id') NOT NULL REFERENCES 'clients_advisors'("client_advisor_id")
+); 
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `account_id` int NOT NULL AUTO_INCREMENT,
+  `balance` float,
+  `client_id` int NOT NULL,
+  PRIMARY KEY (`account_id`),
+  FOREIGN KEY (`client_id`) NOT NULL REFERENCES `clients`(`client_id`)
+); 
+
+DROP TABLE IF EXISTS `clients_accounts`;
+CREATE TABLE `clients_accounts` (
+  `client_account_id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int,
+  'account_id' int,
+  PRIMARY KEY (`client_account_id`)
+  FOREIGN KEY ('account_id') NOT NULL REFERENCES 'accounts'('account_id'),
+  FOREIGN KEY ('client_id') NOT NULL REFERENCES 'clients'('client_id')
+); 
+
+DROP TABLE IF EXISTS `clients_advisors`;
+CREATE TABLE `clients_advisors` (
+  `client_advisor_id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int,
+  'advisor_id' int,
+  PRIMARY KEY (`client_advisor_id`)
+  FOREIGN KEY (`advisor_id`) NOT NULL REFERENCES `clients`(`advisor_id`),
+  FOREIGN KEY (`client_id`) NOT NULL REFERENCES `clients`(`client_id`)
+); 
+
+DROP TABLE IF EXISTS `addresses`;
+CREATE TABLE `addresses` (
+  `address_id` int NOT NULL AUTO_INCREMENT,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `house_number` int,
+  `zip_code` int(5),
+  `client_id` int,
+  PRIMARY KEY (`address_id`),
+  FOREIGN KEY (`client_id`) NOT NULL REFERENCES `clients`(`client_id`)
+); 
+
+DROP TABLE IF EXISTS `financial_advisors`;
+CREATE TABLE `financial_advisors` (
+  'advisor_id' int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  'area_of_expertise' ENUM ('Taxation', 'Estate Planning', 'Portfolio Management'),
+  'client_advisor_id'int,
+  'last_name' varchar(255),
+  'first_name' varchar(255),
+  `client_advisor_id` int,
+  PRIMARY KEY (`address_id`),
+  FOREIGN KEY (`client_advisor_id`) NOT NULL REFERENCES `clients_advisors`(`client_advisor_id`)
+);
+
+SET FOREIGN_KEY_CHECKS=0;
