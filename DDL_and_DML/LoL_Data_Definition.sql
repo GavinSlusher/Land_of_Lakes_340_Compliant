b3e42e89b892b30264bf453e2ec530b76f41e47c
Gavin S <gavinslusher@gmail.com>
2020-06-01 12:18:49 -0400

    pushing updates to data definitions

--- DDL_and_DML/LoL_Data_Definition.sql
+++ DDL_and_DML/LoL_Data_Definition.sql
@@ -8,9 +8,7 @@ CREATE TABLE `addresses` (
   `state` varchar(255) NOT NULL,
   `house_number` int,
   `zip_code` int(5),
-  -- `client_id` int NOT NULL,
   PRIMARY KEY (`address_id`)
-  -- FOREIGN KEY (`client_id`) REFERENCES `clients`(`client_id`)
 );
 
 DROP TABLE IF EXISTS `clients`;
@@ -21,21 +19,15 @@ CREATE TABLE `clients` (
   `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `address_id` int NOT NULL,
-  -- `client_account_id` int NOT NULL,
-  -- `client_advisor_id` int NOT NULL,
   PRIMARY KEY (`client_id`),
   FOREIGN KEY (`address_id`) REFERENCES `addresses`(`address_id`)
-  -- FOREIGN KEY (`client_account_id`) REFERENCES `clients_accounts`(`client_account_id`),
-  -- FOREIGN KEY (`client_advisor_id`) REFERENCES `clients_advisors`(`client_advisor_id`)
 );
 
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `account_id` int NOT NULL AUTO_INCREMENT,
   `balance` float,
-  -- `client_id` int NOT NULL,
   PRIMARY KEY (`account_id`)
-  -- FOREIGN KEY (`client_id`) REFERENCES `clients`(`client_id`)
 );
 
 DROP TABLE IF EXISTS `clients_accounts`;
@@ -45,18 +37,16 @@ CREATE TABLE `clients_accounts` (
   `account_id` int NOT NULL,
   PRIMARY KEY (`client_account_id`),
   FOREIGN KEY (`client_id`) REFERENCES `clients`(`client_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `accounts`(`account_id`)
+  FOREIGN KEY (`account_id`) REFERENCES `accounts`(`account_id`) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS `financial_advisors`;
 CREATE TABLE `financial_advisors` (
   `advisor_id` int NOT NULL AUTO_INCREMENT,
   `area_of_expertise` ENUM('Taxation', 'Estate Planning', 'Portfolio Management'),
-  -- `client_advisor_id` int NOT NULL,
   `first_name` varchar(255),
   `last_name` varchar(255),
   PRIMARY KEY (`advisor_id`)
-  -- FOREIGN KEY (`client_advisor_id`) REFERENCES `clients_advisors`(`client_advisor_id`)
 );
 
 DROP TABLE IF EXISTS `clients_advisors`;
@@ -114,3 +104,24 @@ VALUES ('Portfolio Management', 'Peter', 'Lynch');
 
 INSERT INTO `clients_advisors` (`client_id`, `advisor_id`)
 VALUES ('2', '2');
+
+-- Default Client 3
+INSERT INTO `addresses` (`city`, `state`, `house_number`, `zip_code`)
+VALUES ('Chicago', 'IL', '789', '22222');
+
+INSERT INTO `clients`(`ssn`, `first_name`, `last_name`, `email`, `address_id`)
+VALUES (4567, 'Johnny', 'Tsunami', 'tubular@gmail.com', '3' );
+
+-- NOTE: in the future, can populate address_id with a SQL Query (order by most recent?)
+
+INSERT INTO `accounts` (`balance`)
+VALUES (450.01);
+
+INSERT INTO `clients_accounts` (`client_id`, `account_id`)
+VALUES ('3', '3');
+
+INSERT INTO `financial_advisors` (`area_of_expertise`, `first_name`, `last_name`)
+VALUES ('Portfolio Management', 'Colin', 'Fergeson');
+
+INSERT INTO `clients_advisors` (`client_id`, `advisor_id`)
+VALUES ('3', '3');
